7456363301b9273733ed043ffc4d1ddd81d75154
Sujit Sarkar <sweezit92@gmail.com>
2017-07-10 19:59:26 +0530

    new clickers_sidebar

---
+++ application/views/common/clickers_sidebar.php
@@ -0,0 +1,20 @@
+<ul class="dropdown hover-effect">
+	<li class="dropdown-item active">
+		<a href="author-profile.html">Profile Page</a>
+	</li>
+	<li class="dropdown-item">
+		<a href="author-profile-messages.html">Add Service</a>
+	</li>
+	<li class="dropdown-item">
+		<a href="author-profile-messages.html">Message Board</a>
+	</li>
+	<li class="dropdown-item">
+		<a href="author-profile-followers.html">Ongoing Project</a>
+	</li>
+	<li class="dropdown-item">
+		<a href="author-profile-following.html">Account & Finnace</a>
+	</li>
+	<li class="dropdown-item">
+		<a href="author-badges.html">Feedback & Ratings</a>
+	</li>
+</ul>
