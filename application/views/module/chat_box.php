ffd6da3562dd54be1971cde656322bd4582471de
Sujit Sarkar <sweezit92@gmail.com>
2017-07-13 19:13:17 +0530

    dedes

---
+++ application/views/module/chat_box.php
@@ -0,0 +1,283 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php
+		$this->load->view("common/meta_links");
+	?>
+	<title>Emerald Dragon | Chat Box</title>
+</head>
+<body>
+	
+
+
+	<!-- HEADER -->
+	<?php
+		$this->load->view("common/header");
+	?>
+	<!-- /HEADER -->
+
+	<!-- SIDE MENU -->
+	
+	<!-- /SIDE MENU -->
+
+	<!-- SIDE MENU -->
+	<div id="account-options-menu" class="side-menu right closed">
+		<!-- SVG PLUS -->
+		<svg class="svg-plus">
+			<use xlink:href="#svg-plus"></use>
+		</svg>
+		<!-- /SVG PLUS -->
+
+		<!-- SIDE MENU HEADER -->
+		<div class="side-menu-header">
+			<!-- USER QUICKVIEW -->
+			<div class="user-quickview">
+				<!-- USER AVATAR -->
+				<a href="author-profile.html">
+				<div class="outer-ring">
+					<div class="inner-ring"></div>
+					<figure class="user-avatar">
+						<img src="<?php echo base_url();?>images/avatar_01.jpg" alt="avatar">
+					</figure>
+				</div>
+				</a>
+				<!-- /USER AVATAR -->
+
+				<!-- USER INFORMATION -->
+				<p class="user-name">Johnny Fisher</p>
+				<p class="user-money">$745.00</p>
+				<!-- /USER INFORMATION -->
+			</div>
+			<!-- /USER QUICKVIEW -->
+		</div>
+		<!-- /SIDE MENU HEADER -->
+
+		<!-- SIDE MENU TITLE -->
+		
+		<!-- /SIDE MENU TITLE -->
+
+		<!-- DROPDOWN -->
+		
+	</div>
+	<!-- /SIDE MENU -->
+
+	<!-- MAIN MENU -->
+	
+	<!-- /MAIN MENU -->
+
+	<!-- SECTION HEADLINE -->
+	<div class="section-headline-wrap">
+		<div class="section-headline">
+			<h2>Author's Profile</h2>
+			<p>Home<span class="separator">/</span><span class="current-section">Author's Profile</span></p>
+		</div>
+	</div>
+	<!-- /SECTION HEADLINE -->
+
+	<!-- AUTHOR PROFILE BANNER -->
+	<div class="author-profile-banner"></div>
+	<!-- /AUTHOR PROFILE BANNER -->
+
+	<!-- AUTHOR PROFILE META -->
+	<div class="author-profile-meta-wrap">
+		<div class="author-profile-meta">
+			<!-- AUTHOR PROFILE INFO -->
+			<div class="author-profile-info">
+				<!-- AUTHOR PROFILE INFO ITEM -->
+				<div class="author-profile-info-item">
+					<p class="text-header">Member Since:</p>
+					<p>December 26th, 2013</p>
+				</div>
+				<!-- /AUTHOR PROFILE INFO ITEM -->
+
+				<!-- AUTHOR PROFILE INFO ITEM -->
+				<div class="author-profile-info-item">
+					<p class="text-header">Total Sales:</p>
+					<p>820</p>
+				</div>
+				<!-- /AUTHOR PROFILE INFO ITEM -->
+
+				<!-- AUTHOR PROFILE INFO ITEM -->
+				<div class="author-profile-info-item">
+					<p class="text-header">Freelance Work:</p>
+					<p>Available</p>
+				</div>
+				<!-- /AUTHOR PROFILE INFO ITEM -->
+
+				<!-- AUTHOR PROFILE INFO ITEM -->
+				<div class="author-profile-info-item">
+					<p class="text-header">Website:</p>
+					<p><a href="http://www.odindesign-themes.com/" class="primary">www.odindesign-themes.com</a></p>
+				</div>
+				<!-- /AUTHOR PROFILE INFO ITEM -->
+			</div>
+			<!-- /AUTHOR PROFILE INFO -->
+		</div>
+	</div>
+	<!-- /AUTHOR PROFILE META -->
+
+	<!-- SECTION -->
+	<div class="section-wrap">
+		<div class="section overflowable">
+			<!-- SIDEBAR -->
+			<div class="sidebar left author-profile">
+				<!-- SIDEBAR ITEM -->
+				
+				<!-- /SIDEBAR ITEM -->
+
+				<!-- DROPDOWN -->
+				<?php
+					$this->load->view("common/clickers_sidebar");
+				?>
+				<!-- /DROPDOWN -->
+
+				<!-- SIDEBAR ITEM -->
+				
+				<!-- /SIDEBAR ITEM -->
+			</div>
+			<!-- /SIDEBAR -->
+
+			<!-- CONTENT -->
+			<div class="content right">
+				
+				<div class="headline buttons primary">
+					 <h4> Inbox</h4>
+					<a href="#new-message-popup" class="button mid-short secondary open-new-message">Back To Messsage Board</a>
+				</div>
+			
+				
+            <!-- HEADLINE -->
+          
+            <!-- /HEADLINE -->
+
+            <!-- INBOX MESSAGES PREVIEW -->
+               
+                <!-- INBOX MESSAGE PREVIEW -->
+                <div class="inbox-messages-preview full">
+                <!-- INBOX MESSAGE PREVIEW -->
+                <div class="inbox-message-preview">
+                    <div class="inbox-message-preview-header">
+                        <p class="text-header">
+                            Product Question
+                            <img src="<?php echo base_url();?>images/star-filled.png" alt="star-icon">
+                        </p>
+                    </div>
+
+                    <div class="inbox-message-preview-body">
+						<!-- COMMENT LIST -->
+						<div class="comment-list">
+							<!-- COMMENT -->
+							<div class="comment-wrap">
+								<!-- USER AVATAR -->
+								<a href="user-profile.html">
+									<figure class="user-avatar medium">
+										<img src="<?php echo base_url();?>images/avatar_06.jpg" alt="">
+									</figure>
+								</a>
+								<!-- /USER AVATAR -->
+								<div class="comment">
+									<p class="text-header">Sarah-Imaginarium</p>
+									<p class="timestamp">May 18th, 2014 - 8:22PM</p>
+									<a href="#" class="report">Report</a>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+								</div>
+							</div>
+							<!-- /COMMENT -->
+
+							<!-- LINE SEPARATOR -->
+							<hr class="line-separator">
+							<!-- /LINE SEPARATOR -->
+
+							<!-- COMMENT -->
+							<div class="comment-wrap">
+								<!-- USER AVATAR -->
+								<a href="user-profile.html">
+									<figure class="user-avatar medium">
+										<img src="<?php echo base_url();?>images/avatar_01.jpg" alt="">
+									</figure>
+								</a>
+								<!-- /USER AVATAR -->
+								<div class="comment">
+									<p class="text-header">Johnny Fisher</p>
+									<p class="timestamp">May 19th, 2014 - 2:14PM</p>
+									<a href="#" class="report">Report</a>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magnada. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+								</div>
+							</div>
+							<!-- /COMMENT -->
+
+							<!-- LINE SEPARATOR -->
+							<hr class="line-separator">
+							<!-- /LINE SEPARATOR -->
+
+							<!-- COMMENT -->
+							<div class="comment-wrap">
+								<!-- USER AVATAR -->
+								<a href="user-profile.html">
+									<figure class="user-avatar medium">
+										<img src="<?php echo base_url();?>images/avatar_06.jpg" alt="">
+									</figure>
+								</a>
+								<!-- /USER AVATAR -->
+								<div class="comment">
+									<p class="text-header">Sarah-Imaginarium</p>
+									<p class="timestamp">May 19th, 2014 - 3:47PM</p>
+									<a href="#" class="report">Report</a>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br><br>
+									Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+								</div>
+							</div>
+							<!-- /COMMENT -->
+
+							<!-- LINE SEPARATOR -->
+							<hr class="line-separator">
+							<!-- /LINE SEPARATOR -->
+
+							<!-- COMMENT REPLY -->
+							<div class="comment-wrap comment-reply">
+								<!-- USER AVATAR -->
+								<a href="user-profile.html">
+									<figure class="user-avatar medium">
+										<img src="<?php echo base_url();?>images/avatar_01.jpg" alt="">
+									</figure>
+								</a>
+								<!-- /USER AVATAR -->
+
+								<!-- STYLE TOOL BAR -->
+								
+								<!-- /STYLE TOOL BAR -->
+
+								<!-- COMMENT REPLY FORM -->
+								<form class="comment-reply-form">
+									<textarea name="treply" placeholder="Write your reply here..."></textarea>
+									<button class="button secondary">Post Reply</button>
+								</form>
+								<!-- /COMMENT REPLY FORM -->
+							</div>
+							<!-- /COMMENT REPLY -->
+						</div>
+						<!-- /COMMENT LIST -->
+                    </div>
+                </div>
+                <!-- /INBOX MESSAGE PREVIEW -->
+            </div>
+                <!-- /INBOX MESSAGE PREVIEW -->
+            
+            <!-- /INBOX MESSAGES PREVIEW -->
+        
+				<!-- /COMMENTS -->
+			</div>
+			<!-- CONTENT -->
+
+			<div class="clearfix"></div>
+		</div>
+	</div>
+	<!-- /SECTION -->
+
+	<!-- FOOTER -->
+	<?php
+		$this->load->view("common/footer");
+	?>
+</body>
+</html>
--- application/views/module/clickers_message.php
+++ application/views/module/clickers_message.php
@@ -13,325 +13,7 @@
 	<?php
 		$this->load->view("common/header");
 	?>
-	<!-- /HEADER -->
 
-	<!-- SIDE MENU -->
-	<div id="mobile-menu" class="side-menu left closed">
-		<!-- SVG PLUS -->
-		<svg class="svg-plus">
-			<use xlink:href="#svg-plus"></use>
-		</svg>
-		<!-- /SVG PLUS -->
-
-		<!-- SIDE MENU HEADER -->
-		<div class="side-menu-header">
-			<figure class="logo small">
-				<img src="<?php echo base_url();?>images/logo.png" alt="logo">
-			</figure>
-		</div>
-		<!-- /SIDE MENU HEADER -->
-
-		<!-- SIDE MENU TITLE -->
-		<p class="side-menu-title">Main Links</p>
-		<!-- /SIDE MENU TITLE -->
-
-		<!-- DROPDOWN -->
-		<ul class="dropdown dark hover-effect interactive">
-			<!-- DROPDOWN ITEM -->
-			<li class="dropdown-item">
-				<a href="index.html">Home</a>
-			</li>
-			<!-- /DROPDOWN ITEM -->
-
-			<!-- DROPDOWN ITEM -->
-			<li class="dropdown-item">
-				<a href="how-to-shop.html">How to Shop</a>
-			</li>
-			<!-- /DROPDOWN ITEM -->
-
-			<!-- DROPDOWN ITEM -->
-			<li class="dropdown-item">
-				<a href="products.html">Products</a>
-			</li>
-			<!-- /DROPDOWN ITEM -->
-
-			<!-- DROPDOWN ITEM -->
-			<li class="dropdown-item">
-				<a href="services.html">Services</a>
-			</li>
-			<!-- /DROPDOWN ITEM -->
-
-			<!-- DROPDOWN ITEM -->
-			<li class="dropdown-item">
-				<a href="shop-gridview-v1.html">Online Goods</a>
-			</li>
-			<!-- /DROPDOWN ITEM -->
-
-			<!-- DROPDOWN ITEM -->
-			<li class="dropdown-item interactive">
-				<a href="#">
-					Features
-					<!-- SVG ARROW -->
-					<svg class="svg-arrow">
-						<use xlink:href="#svg-arrow"></use>
-					</svg>
-					<!-- /SVG ARROW -->
-				</a>
-
-				<!-- INNER DROPDOWN -->
-				<ul class="inner-dropdown">
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<p>Emerald Dragon</p>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="index.html">Homepage V1</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="home-v2.html">Homepage V2</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="itemview-versions.html">Item View Versions</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="shop-gridview-v1.html">Shop Grid View V1</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="shop-gridview-v2.html">Shop Grid View V2</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="shop-listview-v1.html">Shop List View V1</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="shop-listview-v2.html">Shop List View V2</a>
-						<!-- PIN -->
-						<span class="pin soft-edged primary">hot</span>
-						<!-- /PIN -->
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="author-profile.html">Profile Page</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="blog-v1.html">Blog Page V1</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="blog-v2.html">Blog Page V2</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="open-post.html">Open Post</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="forum.html">Forum Board</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="subforum.html">Subforum</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="open-topic.html">Open Topic</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="login-register.html">Login and Register</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="menu-dropdowns.html">Menu and Dropdowns</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<p>Product Pages</p>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="item-v1.html">Item Page V1</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="item-v2.html">Item Page V2</a>
-						<!-- PIN -->
-						<span class="pin soft-edged secondary">new</span>
-						<!-- /PIN -->
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="product-page.html">Product Page</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="auction-page.html">Auction Page</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="service-page.html">Service Page</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="favourites.html">Favourite Products Grid View</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="favourites-listview.html">Favourite Products List View</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="alerts-notifications.html">Alerts &amp; Notifications</a>
-						<!-- PIN -->
-						<span class="pin soft-edged violet">new</span>
-						<!-- /PIN -->
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<p>Dashboard</p>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="dashboard-settings.html">Account Settings</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="dashboard-statistics.html">Statistics Page</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="dashboard-statement.html">Sales Statement</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="dashboard-inbox.html">Inbox Page</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="dashboard-openmessage.html">Open Message</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="dashboard-uploaditem.html">Upload Page</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<p>Gamification</p>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="community-badges.html">Author Badges Page</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="badges.html">All Badges (Big and Small)</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="flag-badges.html">Flag Badges (Big and Small)</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="badges-boxes.html">Badge Boxes Versions</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-
-					<!-- INNER DROPDOWN ITEM -->
-					<li class="inner-dropdown-item">
-						<a href="author-badges.html">Public Author Badges</a>
-					</li>
-					<!-- /INNER DROPDOWN ITEM -->
-				</ul>
-				<!-- /INNER DROPDOWN -->
-			</li>
-			<!-- /DROPDOWN ITEM -->
-		</ul>
-		<!-- /DROPDOWN -->
-	</div>
-	<!-- /SIDE MENU -->
-
-	<!-- SIDE MENU -->
 	<div id="account-options-menu" class="side-menu right closed">
 		<!-- SVG PLUS -->
 		<svg class="svg-plus">
@@ -363,96 +45,7 @@
 		</div>
 		<!-- /SIDE MENU HEADER -->
 
-		<!-- SIDE MENU TITLE -->
-		<p class="side-menu-title">Your Account</p>
-		<!-- /SIDE MENU TITLE -->
 
-		<!-- DROPDOWN -->
-		<ul class="dropdown dark hover-effect">
-			<!-- DROPDOWN ITEM -->
-			<li class="dropdown-item">
-				<a href="dashboard-notifications.html">Notifications</a>
-			</li>
-			<!-- /DROPDOWN ITEM -->
-
-			<!-- DROPDOWN ITEM -->
-			<li class="dropdown-item">
-				<a href="dashboard-inbox.html">Messages</a>
-			</li>
-			<!-- /DROPDOWN ITEM -->
-
-			<!-- DROPDOWN ITEM -->
-			<li class="dropdown-item">
-				<a href="cart.html">Your Cart</a>
-			</li>
-			<!-- /DROPDOWN ITEM -->
-
-			<!-- DROPDOWN ITEM -->
-			<li class="dropdown-item">
-				<a href="favourites.html">Favourites</a>
-			</li>
-			<!-- /DROPDOWN ITEM -->
-		</ul>
-		<!-- /DROPDOWN -->
-
-		<!-- SIDE MENU TITLE -->
-		<p class="side-menu-title">Dashboard</p>
-		<!-- /SIDE MENU TITLE -->
-
-		<!-- DROPDOWN -->
-		<ul class="dropdown dark hover-effect">
-			<!-- DROPDOWN ITEM -->
-			<li class="dropdown-item">
-				<a href="author-profile.html">Profile Page</a>
-			</li>
-			<!-- /DROPDOWN ITEM -->
-
-			<!-- DROPDOWN ITEM -->
-			<li class="dropdown-item">
-				<a href="dashboard-settings.html">Account Settings</a>
-			</li>
-			<!-- /DROPDOWN ITEM -->
-
-			<!-- DROPDOWN ITEM -->
-			<li class="dropdown-item">
-				<a href="dashboard-purchases.html">Your Purchases</a>
-			</li>
-			<!-- /DROPDOWN ITEM -->
-
-			<!-- DROPDOWN ITEM -->
-			<li class="dropdown-item">
-				<a href="dashboard-statement.html">Sales Statement</a>
-			</li>
-			<!-- /DROPDOWN ITEM -->
-
-			<!-- DROPDOWN ITEM -->
-			<li class="dropdown-item">
-				<a href="dashboard-buycredits.html">Buy Credits</a>
-			</li>
-			<!-- /DROPDOWN ITEM -->
-
-			<!-- DROPDOWN ITEM -->
-			<li class="dropdown-item">
-				<a href="dashboard-withdrawals.html">Withdrawals</a>
-			</li>
-			<!-- /DROPDOWN ITEM -->
-
-			<!-- DROPDOWN ITEM -->
-			<li class="dropdown-item">
-				<a href="dashboard-uploaditem.html">Upload Item</a>
-			</li>
-			<!-- /DROPDOWN ITEM -->
-
-			<!-- DROPDOWN ITEM -->
-			<li class="dropdown-item">
-				<a href="dashboard-manageitems.html">Manage Items</a>
-			</li>
-			<!-- /DROPDOWN ITEM -->
-		</ul>
-		<!-- /DROPDOWN -->
-
-		<a href="#" class="button medium secondary">Logout</a>
-		<a href="#" class="button medium primary">Become a Seller</a>
 	</div>
 	<!-- /SIDE MENU -->
 
@@ -517,25 +110,6 @@
 			<!-- SIDEBAR -->
 			<div class="sidebar left author-profile">
 				<!-- SIDEBAR ITEM -->
-				<div class="sidebar-item author-bio">
-					<!-- USER AVATAR -->
-					<a href="user-profile.html" class="user-avatar-wrap medium">
-						<figure class="user-avatar medium">
-							<img src="<?php echo base_url();?>images/avatar_09.jpg" alt="">
-						</figure>
-					</a>
-					<!-- /USER AVATAR -->
-					<p class="text-header">Odin_Design</p>
-					<p class="text-oneline">Super Samurai Developers<br>Los Angeles, Usa</p>
-					<!-- SHARE LINKS -->
-					<ul class="share-links">
-						<li><a href="#" class="fb"></a></li>
-						<li><a href="#" class="twt"></a></li>
-						<li><a href="#" class="db"></a></li>
-					</ul>
-					<!-- /SHARE LINKS -->
-					<a href="#" class="button mid dark-light">Send a Private Message</a>
-				</div>
 				<!-- /SIDEBAR ITEM -->
 
 				<!-- DROPDOWN -->
@@ -581,7 +155,9 @@
 							<!-- /PIN -->
 							<p class="timestamp">5 Hours Ago</p>
 							<a href="#" class="report">Report</a>
-							<p>I,ve recently bought your theme and let me say it,s fantastic! I have a small question regarding the main files and how to install the theme. Could you help me? Thanks!</p>
+							<a href="<?php echo base_url()?>index.php/chat_box">
+								<p>I,ve recently bought your theme and let me say it,s fantastic! I have a small question regarding the main files and how to install the theme. Could you help me? Thanks!</p>
+							</a>
 						</div>
 					</div>
 					<!-- /COMMENT -->
@@ -603,7 +179,9 @@
 							<p class="text-header">Cloud Templates</p>
 							<p class="timestamp">8 Hours Ago</p>
 							<a href="#" class="report">Report</a>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magnada. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+							<a href="<?php echo base_url()?>index.php/chat_box">
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magnada. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+							</a>
 						</div>
 					</div>
 					<!-- /COMMENT -->
@@ -620,7 +198,9 @@
 							<p class="text-header">Lucy Diamond</p>
 							<p class="timestamp">10 Hours Ago</p>
 							<a href="#" class="report">Report</a>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magnada. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+							<a href="<?php echo base_url()?>index.php/chat_box">
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magnada. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+							</a>
 						</div>
 					</div>
 
