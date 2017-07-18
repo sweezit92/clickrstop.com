f3ac82f69d8c62b9fd7faad6faaa8ed55b2095fe
Deep Bratt Mondal <deepbratt@gmail.com>
2017-07-18 12:27:15 +0530

    pic upload
    saasda

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -5,7 +5,7 @@
 				<ul class="main-menu">
 					<!-- MENU ITEM -->
 					<li class="menu-item active" style="margin:0px;">
-						<a href="<?php echo base_url();?>" class="active" style="margin:0px;padding:0px;"><img src="<?php echo base_url();?>images/Screenshot_6.png" style="height:60px;width:200px;margin:0px;padding:0px;"></a>
+						<a href="<?php echo base_url();?>" class="active" style="margin:0px;padding:0px;"><img src="<?php echo base_url();?>images/logora2.png" style="height:70px;margin:0px;padding:0px;"></a>
 					</li>
 					<!-- /MENU ITEM -->
 				
